cbb19ff51c5abee264e04265f93253f7606da196
root <root@xxxxx>
2017-09-17 18:42:28 +0200

    xx

--- core.ino
+++ core.ino
@@ -10,9 +10,14 @@ TODO:
 
  ////WED 13/9 PM: muon all workings
 
-- test cores with new code - basics of read and write...
-- put new core code in interrupt
-- scanning of cores and serial out
+- test cores with new code - basics of read and write:
+
+no luck - 5v we have, logic PWR we have, sense is connected DONE - timing might need some tweaks
+
+- put new core code in interrupt and finalise all - problems - retested muon code all good
+- do core simulationDONE
+
+- scanning of cores and serial outDONE
 
 /////
 
@@ -37,12 +42,6 @@ cores set to this state by forcing them to the opposite state and
 watching for the kickback pulse.  It you see the pulse the bit was in
 the "1" state.  if not, it was already in the "0" state.
 
-ENABLES: left hand - top to bottom X0-X7 Y0-Y7
-
-X: middle left to right HIX7-HIX0 LOX7-LOX0
-
-Y: lower left to right HIY7-HIY0 LOY7-LOY0
-
 so WR is HIX0 and HIY0 high - D3 and D5 on PORTD -> arduino Digital 0-7
 RD is LOX0 and LOY0 high - D4 and D6 on PORTD
 
@@ -54,6 +53,7 @@ this is for test code: sense is on PORTB 8-13 - so pin 8
 // port mappings
 // with 0=A,1=B,2=C,3=D,4=E,5=G,6=H,7=L
 
+//ENABLES: left hand - top to bottom X0-X7 Y0-Y7
 byte enable_pins_x[8]={4,5,5,3, 3,4,5,6}; // E,E,G,E, H,H,H,H // digital pins 2-9
 byte enable_port_x[8]={4,4,5,4, 6,6,6,6};
 byte enable_pins_y[8]={4,5,6,7, 0,1,2,3}; // B,B,B,B, D,D,D,D // digital pins 10,11,12,13 comm 21,20,19,18
@@ -62,16 +62,21 @@ byte enable_port_y[8]={1,1,1,1, 3,3,3,3}; // checked!
 // sense will be: say 14 = PJ1
 
 // Arduino end double row connector - working from top left down:
+// X: middle left to right HIX7-HIX0 LOX7-LOX0 - so reverse order of theseDONE
+byte hi_x_pins[8]= {1,3,5,7,6,4,2,0};  // A,A,A,A, C,C,C,C // middle left to right HIX7-HIX0 on core board. top left on arduino - REV
+byte hi_x_port[8]= {2,2,2,2, 0,0,0,0};//rev
 
-byte hi_x_pins[8]= {0,2,4,6, 7,5,3,1};  // A,A,A,A, C,C,C,C // middle left to right HIX7-HIX0 on core board. top left on arduino 
-byte hi_x_port[8]= {0,0,0,0, 2,2,2,2};
-byte low_x_pins[8]={7,1,7,5, 3,1,3,1}; // D,G,L,L, L,L,B,B// LOX7-LOX0
-byte low_x_port[8]={3,5,7,7, 7,7,1,1}; // checked!
+byte low_x_pins[8]={1,3,1,3,5,7,1,7}; // D,G,L,L, L,L,B,B// LOX7-LOX0 rev
+byte low_x_port[8]={1,1,7,7,7,7,5,3}; // checked! rev
 
-byte hi_y_pins[8]= {1,3,5,7, 6,4,2,0}; // A,A,A,A, C,C,C,C// lower left to right HIY7-HIY0. top right down on arduino
-byte hi_y_port[8]= {0,0,0,0, 2,2,2,2};
-byte low_y_pins[8]={2,0,6,4, 2,0,2,0}; // G,G,L,L, L,L,B,B // LOY7-LOY0
-byte low_y_port[8]={5,5,7,7, 7,7,1,1}; // checked
+//Y: lower left to right HIY7-HIY0 LOY7-LOY0
+byte hi_y_pins[8]= {0,2,4,6,7,5,3,1}; // rev A,A,A,A, C,C,C,C// lower left to right HIY7-HIY0. top right down on arduino
+byte hi_y_port[8]= {2,2,2,2, 0,0,0,0}; // rev
+
+byte low_y_pins[8]={0,2,0,2,4,6,0,2}; // rev G,G,L,L, L,L,B,B // LOY7-LOY0
+byte low_y_port[8]={1,1,7,7,7,7,5,5}; // rev checked
+
+
 
 //#define  PULSE  6 
 #define  DELAY  10
@@ -85,6 +90,148 @@ byte low_y_port[8]={5,5,7,7, 7,7,1,1}; // checked
 #define  RD    0x50 // RD is D4 and D6 high 
 
 volatile unsigned char pulsed, xco, yco, xcc,ycc;
+volatile byte cores[8][8]={0};
+
+    void sDelay (byte cnt) {
+      for (byte ii = 0; ii < cnt; ii++) {
+        __asm__ ("nop\n\t");    // NOP delays 62.5 ns
+        __asm__ ("nop\n\t");
+      }
+    }
+
+
+inline byte pulserl(byte x, byte y){
+  byte a,b,c,d;  
+  c=1<<low_x_pins[x];
+  d=1<<low_y_pins[y];
+  switch(low_x_port[x]) {
+    case 1:
+      PORTB=c;
+      break;
+    case 3:
+      PORTD=c;
+      break;
+    case 5:
+      PORTG=c;
+      break;
+    case 7:
+      PORTL=c;
+    }
+
+    switch(low_y_port[y]) {
+    case 1:
+      PORTB|=d;
+      break;
+    case 5:
+      PORTG|=d;
+      break;
+    case 7:
+      PORTL|=d;
+    }     
+  a=1<<enable_pins_x[x];
+  b=1<<enable_pins_y[y];
+
+  //  if (enable_port_x[x]==4)     PORTE=a;
+  //  else if (enable_port_x[x]==5) PORTG|=a;
+  //  else PORTH=a;
+
+  if (enable_port_y[y]==1 && enable_port_x[x]==4)     {
+    PORTB|=b; PORTE=a;
+  }
+  else if (enable_port_y[y]==1 && enable_port_x[x]==5)     {
+    PORTB|=b; PORTG|=a;
+  }
+  else if (enable_port_y[y]==1 && enable_port_x[x]==6)     {
+    PORTB|=b; PORTH=a;
+  }
+  else if (enable_port_y[y]==3 && enable_port_x[x]==4)     {
+    PORTD|=b; PORTE=a;
+  }
+  else if (enable_port_y[y]==3 && enable_port_x[x]==5)     {
+    PORTD|=b; PORTG|=a;
+  }
+  else if (enable_port_y[y]==3 && enable_port_x[x]==6)     {
+    PORTD|=b; PORTH=a;
+  }
+    
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+  //  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\t");
+  //  __asm__ ("nop\n\t");
+    
+
+  // sample sense amp output after ~1,200 ns delay
+  byte smpl = PINJ & 0x02; // sense is on 14 = PJ1 - checked 
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  // 
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  //
+  
+  // clear enables - all ports?
+  PORTB=0; PORTD=0; PORTG=0; PORTH=0; PORTL=0; PORTE=0; 
+  sDelay(DELAY);
+    return smpl>>1;
+}
+  
+  // set enable on for x and for y
+// with 0=A,1=B,2=C,3=D,4=E,5=G,6=H,7=L
+
+//byte enable_port_x[8]={4,4,5,4, 6,6,6,6};
+//byte enable_port_y[8]={1,1,1,1, 3,3,3,3}; // checked!
+
+  
+
+inline byte pulserh(byte x, byte y){
+  byte a,b,c,d;  
+  c=1<<hi_x_pins[x];
+  d=1<<hi_y_pins[y];
+  
+  if (hi_x_port[x]==0)      PORTA=c;
+  else PORTC=c;
+
+  if (hi_y_port[y]==0)      PORTA|=d;
+    else PORTC|=d;
+
+    a=1<<enable_pins_x[x];
+    b=1<<enable_pins_y[y];
+
+  if (enable_port_y[y]==1 && enable_port_x[x]==4)     {
+    PORTB=b; PORTE=a;
+  }
+  else if (enable_port_y[y]==1 && enable_port_x[x]==5)     {
+    PORTB=b; PORTG=a;
+  }
+  else if (enable_port_y[y]==1 && enable_port_x[x]==6)     {
+    PORTB=b; PORTH=a;
+  }
+  else if (enable_port_y[y]==3 && enable_port_x[x]==4)     {
+    PORTD=b; PORTE=a;
+  }
+  else if (enable_port_y[y]==3 && enable_port_x[x]==5)     {
+    PORTD=b; PORTG=a;
+  }
+  else if (enable_port_y[y]==3 && enable_port_x[x]==6)     {
+    PORTD=b; PORTH=a;
+  }
+
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+  //  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+    __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\t");
+
+  //  __asm__ ("nop\n\tnop\n\t");
+  //  __asm__ ("nop\n\t");
+
+  
+  // sample sense amp output after ~1,200 ns delay
+  byte smpl = PINJ & 0x02; // sense is on 14 = PJ1 - checked 
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  // 
+  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  //
+  
+  // clear enables - all ports?
+  PORTA=0; PORTB=0; PORTC=0; PORTD=0; PORTE=0; PORTG=0; PORTH=0;
+  sDelay(DELAY);
+    return smpl>>1;
+}
 
 void pciSetup(byte pin)
 {
@@ -97,20 +244,91 @@ void pciSetup(byte pin)
 
 ISR (PCINT2_vect) // handle pin change interrupt for D0 to D7 here - also for PORTK
  {
+      byte posx, posy=0;
+      byte x, y;
    // xcc and ycc and pulsed are the goals
       //      x=255-PINK; // 255 is all pins doing nothing = 5v high... now all drop...
       //      x=255-PINK;
    xco=PINK;yco=PINF;
-   if (xco!=255 && yco!=255 && pulsed!=1){
+   // test muon code 
+      if (xco!=255 && yco!=255 && pulsed!=1){
+     xcc=255-xco;
+     ycc=255-yco;
      pulsed=1;
-     xcc=255-xco;ycc=255-yco;
-   }
+     }
+
+   // XXXXXX___________>>>>                   simulated core code
+   /*
+ if (xco!=255 && yco!=255){
+     x=255-xco;y=255-yco;
+     // figure out which core we want
+     // if pulsed then decode address from bits in x and y
+     // eg. 0100 0000 and we want 1 1=0, 2=1, 3=2
+	byte count = 0;
+	while (x)
+	  {
+	    x = x >> 1;
+	    ++count;
+	  }
+	posx=count;
+
+	count=0;
+	while (y)
+	  {
+	    y = y >> 1;
+	    ++count;
+	  }
+	posy=count;
+	//      pulsed=0;
+      posx-=1; posy-=1;
+      // then flip that bit - pulseh, pulsel
+      // sense output and flip it so do write again if there is no sense out
+      // if we write 1 and was a 0 then we sense flip =0 so we don't flip
+      // if we write 1 and was a 1 then we sense no flip so then we need to flip it - write a 0
+      cores[posx][posy]^=1; // flips
+      }
+   */   
+
+   //??????????????????????????????????????????????????????????    full core code
+      /*   
+       if (xco!=255 && yco!=255){
+     x=255-xco;y=255-yco;
+     // figure out which core we want
+     // if pulsed then decode address from bits in x and y
+     // eg. 0100 0000 and we want 1 1=0, 2=1, 3=2
+	byte count = 0;
+	while (x)
+	  {
+	    x = x >> 1;
+	    ++count;
+	  }
+	posx=count;
+
+	count=0;
+	while (y)
+	  {
+	    y = y >> 1;
+	    ++count;
+	  }
+	posy=count;
+	//      pulsed=0;
+      posx-=1; posy-=1;
+      // then flip that bit - pulseh, pulsel
+      // sense output and flip it so do write again if there is no sense out
+      // if we write 1 and was a 0 then we sense flip =0 so we don't flip
+      // if we write 1 and was a 1 then we sense no flip so then we need to flip it - write a 0
+
+      if (pulserh(posx,posy)==1) { // it didn't flip so it was a 1 and now needs to be a zero
+
+	pulserl(posx,posy); // a flip is a 0 - so no flip then flip
+	}
+	}*/
  }
 
 void setup() {
   Serial.begin(9600);
   //  DDRD = DDRD | 0xFC;  // D2-D7 are outputs - so far these just for testings
-  //  DDRB = DDRB | 0xFE;  // D8 is input
+  //  DDRB = DDRB | 0xFE;  // D8 is input = b0
 
   // inputs for muon = no pullups
   DDRF = 0;
@@ -126,10 +344,19 @@ void setup() {
   DDRB=255; PORTB=0;
   DDRC=255; PORTC=0;
   DDRD=255; PORTD=0;
-  DDRE=255; PORTE=0; // this works for serial
+  DDRE=254; PORTE=0; // this works for serial
   DDRG=255; PORTG=0;
   DDRH=255; PORTH=0;
   DDRL=255; PORTL=0;
+
+  // zero all cores
+  byte corex,corey;
+        for (corey=0;corey<8;corey++){
+      for (corex=0;corex<8;corex++){
+	  pulserl(corex,corey); 
+	  delay(10);
+      }
+	}
   
   pciSetup(A8); // portk inputs - make sure these are the ones on top of pyramid A8->
   pciSetup(A9);
@@ -138,224 +365,24 @@ void setup() {
   pciSetup(A12);
   pciSetup(A13);
   pciSetup(A14);
-  pciSetup(A15); 
-}
+  pciSetup(A15);
 
-    void sDelay (byte cnt) {
-      for (byte ii = 0; ii < cnt; ii++) {
-        __asm__ ("nop\n\t");    // NOP delays 62.5 ns
-        __asm__ ("nop\n\t");
-      }
-    }
-
-    byte pulse (byte lines) {
-      PORTD = (lines + EN);
-      __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-      __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-      __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-      __asm__ ("nop\n\t");
-      
-      //__asm__ ("nop\n\t"); // added extra delay line
-      //  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t"); // added extra delay line
-      //  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t"); // added extra delay line
-
-      // sample sense amp output after ~1,200 ns delay
-       byte smpl = PINB & 0x01; // sense is on PB1 whcih is d8
-      __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  // 
-      __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  //
-      //      __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  // extra
-      PORTD = 0x00;
-      sDelay(DELAY);
-      return smpl;
-    }
-
-inline byte pulser(byte x, byte y, byte what){
   
-  // set x and y
 
-  if (what==1){
-// so for a write 1 we want hi_x + hi_y =1
-//byte hi_x_port[8]= {0,0,0,0, 2,2,2,2};
-    switch(hi_x_port[x]) {
-    case 0:
-      PORTA|=1<<hi_x_pins[x];
-      break;
-    case 2:
-      PORTC|=1<<hi_x_pins[x];
-    }
-
-    // byte hi_y_port[8]= {0,0,0,0, 2,2,2,2};
-
-    switch(hi_y_port[y]) {
-    case 0:
-      PORTA|=1<<hi_y_pins[y];
-      break;
-    case 2:
-      PORTC|=1<<hi_y_pins[y];
-    }     
-  }
-  else
-    {
-// for a write 0 we want lo_x + lo_y =1
-//byte low_x_port[8]={3,5,7,7, 7,7,1,1}; // checked!
-    switch(low_x_port[x]) {
-    case 1:
-      PORTB|=1<<low_x_pins[x];
-      break;
-    case 3:
-      PORTD|=1<<low_x_pins[x];
-      break;
-    case 5:
-      PORTG|=1<<low_x_pins[x];
-      break;
-    case 7:
-      PORTL|=1<<low_x_pins[x];
-    }
-    //byte low_y_port[8]={5,5,7,7, 7,7,1,1}; // checked
-
-    switch(low_y_port[y]) {
-    case 1:
-      PORTB|=1<<low_y_pins[y];
-      break;
-    case 5:
-      PORTG|=1<<low_y_pins[y];
-      break;
-    case 7:
-      PORTL|=1<<low_y_pins[y];
-    }     
-
-    }
-  
-  // set enable on for x and for y
-// with 0=A,1=B,2=C,3=D,4=E,5=G,6=H,7=L
-
-//byte enable_port_x[8]={4,4,5,4, 6,6,6,6};
-//byte enable_port_y[8]={1,1,1,1, 3,3,3,3}; // checked!
-
-  switch(enable_port_x[x]) {
-  case 4: // PORTE - set bit
-    PORTE|=1<<enable_pins_x[x];
-    break;
-  case 5: // PORTG - set bit
-    PORTG|=1<<enable_pins_x[x];
-    break;
-  case 6: // PORTH - set bit
-    PORTH|=1<<enable_pins_x[x];
-  } 
-
-  switch(enable_port_y[y]) {
-  case 1: // PORTB - set bit
-    PORTB|=1<<enable_pins_y[y];
-    break;
-  case 3: // PORTD - set bit
-    PORTD|=1<<enable_pins_y[y];
-  } 
-  
-  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-  __asm__ ("nop\n\t");
-
-  // sample sense amp output after ~1,200 ns delay
-  byte smpl = PINJ & 0x02; // sense is on 14 = PJ1
-  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  // 
-  __asm__ ("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");  //
-  
-  // clear enables
-
-    switch(enable_port_x[x]) {
-  case 4: // PORTE - clear bit
-    PORTE&= ~(1 << enable_pins_x[x]);
-    break;
-  case 5: // PORTG - clear bit
-    PORTG&= ~(1 << enable_pins_x[x]);
-    break;
-  case 6: // PORTH - clear bit
-    PORTH&= ~(1 << enable_pins_x[x]);
-  } 
-
-  switch(enable_port_y[y]) {
-  case 1: // PORTB - clear bit
-    PORTB&= ~(1 << enable_pins_y[y]);
-    break;
-  case 3: // PORTD - clear bit
-    PORTD&= ~(1 << enable_pins_y[y]);
-  } 
-  
-  // clear all bits what we did set
-
-    if (what==1){
-// so for a write 1 we want hi_x + hi_y =1
-    switch(hi_x_port[x]) {
-    case 0:
-      PORTA&=~(1<<hi_x_pins[x]);
-      break;
-    case 2:
-      PORTC&=~(1<<hi_x_pins[x]);
-    }
-
-    switch(hi_y_port[y]) {
-    case 0:
-      PORTA&=~(1<<hi_y_pins[y]);
-      break;
-    case 2:
-      PORTC&=~(1<<hi_y_pins[y]);
-    }     
-  }
-  else
-    {
-// for a write 0 we want lo_x + lo_y =1
-    switch(low_x_port[x]) {
-    case 1:
-      PORTB&=~(1<<low_x_pins[x]);
-      break;
-    case 3:
-      PORTD&=~(1<<low_x_pins[x]);
-      break;
-    case 5:
-      PORTG&=~(1<<low_x_pins[x]);
-      break;
-    case 7:
-      PORTL&=~(1<<low_x_pins[x]);
-    }
-
-    switch(low_y_port[y]) {
-    case 1:
-      PORTB&=~(1<<low_y_pins[y]);
-      break;
-    case 5:
-      PORTG&=~(1<<low_y_pins[y]);
-      break;
-    case 7:
-      PORTL&=~(1<<low_y_pins[y]);
-    }     
-
-    }  
-    sDelay(DELAY);
-    return smpl;
-}
-
-void flipper (byte x, byte y){
-
-  // sense output and flip it so do write again if there is no sense out
-  // if we write 1 and was a 0 then we sense flip =0 so we don't flip
-  // if we write 1 and was a 1 then we sense no flip so then we need to flip it - write a 0
-
-  if (pulser(x,y,1)==1) pulser(x,y,0); // is a flip a 1 or a 0? a 0 I think so...
   
 }
 
     void loop() {
-      byte posx, posy=0;
-      byte x, y;
+      //      static byte counter=0;
+      // muon test code - now adapt for flipping
       
+      byte x,y, posx, posy, yyy;
       if (pulsed==1 && xcc>0 && ycc>0){
 	noInterrupts();
 	x=xcc;y=ycc;
 	interrupts();
       // if pulsed then decode address from bits in xco and yco
       // eg. 0100 0000 and we want 1 1=0, 2=1, 3=2
-
 	byte count = 0;
 	while (x)
 	  {
@@ -373,24 +400,111 @@ void flipper (byte x, byte y){
 	posy=count;
       pulsed=0;
       posx-=1; posy-=1;
-      // test code to print where muon is x and y
+
+      // test core code
+      yyy=pulserh(posx,posy);
+      if (yyy==1) { // it didn't flip so it was a 1 and now needs to be a zero
+      	pulserl(posx,posy); // a flip is a 0 - so no flip then flip
+      }
+
+      // test code to print where muon is x and y            - JAMIE SERIAL DONE!
+
+            
+       Serial.print("A");
+       Serial.println(posx);
+
+       Serial.print("B");
+       Serial.println(posy);
+
+       Serial.print("C");
+       Serial.println(yyy^1);
+
 
       
-      Serial.print(posx);
+      /*      Serial.print(posx);
       Serial.print(":");
       Serial.print(posy);
-      Serial.println("");
+      Serial.print(":");
+      Serial.print(yyy^1);
+      Serial.println("");*/
       
-      
-      // flip the bit so read and then write at posx and posy
-      // this should be in the interrupt
-      
-      //      flipper(posx, posy); 
-
       }
-      //      if ((255-PINF)&64) Serial.println("XXXXXXXXXXXXXXX"); // testingsxxx
       
+
+      // XXXXXXXXXXXXXXXXXXXXXXXX                simulated core code
+      /*      byte corex=0, corey=0, xyz;
+      for (corex=0;corex<8;corex++){
+      	for (corey=0;corey<8;corey++){
+	  noInterrupts();
+	  Serial.print(cores[corex][corey]);
+	  interrupts();
+	  delay(100);
+	}
+      }
+      Serial.println("");
+      delay(500);
+      */
+            
       // - scan full x and y every x seconds and output this data via serial to PI
+      // X??????????????????????????????????????   code for cores
+      /*
+      byte corex=0, corey=0, xyz;
+      //      counter++;
+      //      if (counter==8){
+      for (corey=0;corey<8;corey++){
+      for (corex=0;corex<8;corex++){
+
+	//	  noInterrupts();
+	  xyz=pulserl(corex,corey); //  0=flip
+	  //	  interrupts();
+
+	  // if it was a 0 then no flip so it was a 0
+	  if (xyz==1) Serial.print("0");// println! // no flip
+	  else {
+	    // if it was a 0 then flipped so it was a 1 and we need to flip back
+	    Serial.print("1");
+
+	    //	    noInterrupts();
+	    pulserh(corex,corey);// println!
+	    //	    interrupts();
+	    delay(10);
+	  }
+	  delay(10);
+          }
+      }
+      Serial.println("");
+      */
+      //      delay(500);
+      //      counter=0;
+      //    }
+
+            // test code for cores - which works fine almost!
+            byte corex=0, corey=0;
+      //      for (corex=0;corex<8;corex++){
+      //      	for (corey=0;corey<8;corey++){
+	    /*      corex=rand()%8;
+      corey=rand()%8;
+      Serial.print(corex);
+      Serial.print(":");
+      Serial.print(corey);
+      Serial.print(":  ");
+	
+      byte xyz=pulserh(corex,corey); 
+      //byte xyz=pulserhigh();// flip to 1=flip to give 001 which is what gives so this works
+      Serial.print(xyz);
+      Serial.print(":");
+      xyz=pulserl(corex,corey); // flip to 0=flip
+      //      xyz=pulserlow();
+      Serial.print(xyz);
+      Serial.print(":");
+      xyz=pulserl(corex,corey); // flip to 0=no flip
+      //      xyz=pulserlow();
+      Serial.print(xyz);
+      Serial.println("");
+      delay(100);*/
+      //          }
+      //        }
+     
 
 
     }
